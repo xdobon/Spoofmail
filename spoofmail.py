6a643941d90a6ade39c3b284fb95d99c1d1d7fef
Xavier Dobon <58508626+xdobon@users.noreply.github.com>
2024-04-25 18:28:06 +0200

    spoofmail.py

--- spoofmail.py
+++ spoofmail.py
@@ -1,35 +1,37 @@
 import smtplib
-from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-# Configuración del servidor SMTP y credenciales
-smtp_server = 'tu_servidor_smtp'
-port = 587  # Puerto para TLS
-sender_email = 'tu_correo@gmail.com'  # Remitente
-receiver_email = 'destinatario@example.com'  # Destinatario
-password = 'tu_contraseña'
+def enviar_correo_spoofed(destinatario, asunto, cuerpo, remitente_falso,remitente_real,password,servidor):
+    # Crear un objeto MIMEMultipart para el correo electrónico
+    mensaje = MIMEMultipart()
+    mensaje['From'] = remitente_falso  # Cambiar el remitente al remitente falso
+    mensaje['To'] = destinatario
+    mensaje['Subject'] = asunto
+    mensaje.attach(MIMEText(cuerpo, 'plain'))
 
-# Crear el objeto del mensaje
-message = MIMEMultipart()
-message['From'] = sender_email
-message['To'] = receiver_email
-message['Subject'] = 'Asunto del correo'
+    # Iniciar la conexión SMTP
+    servidor_smtp = smtplib.SMTP(servidor, 587)
+    servidor_smtp.starttls()
 
-# Cuerpo del mensaje
-body = 'Hola,\n\nEste es un correo de prueba enviado desde Python.'
-message.attach(MIMEText(body, 'plain'))
+    # Iniciar sesión en el servidor SMTP
+    servidor_smtp.login(remitente_real, password)
 
-# Establecer la conexión con el servidor SMTP
-try:
-    server = smtplib.SMTP(smtp_server, port)
-    server.starttls()  # Habilitar seguridad TLS
-    # Iniciar sesión en el servidor SMTP y enviar el correo
-    server.login(sender_email, password)
-    text = message.as_string()
-    server.sendmail(sender_email, receiver_email, text)
-    print('Correo enviado correctamente!')
-except Exception as e:
-    print('Error al enviar el correo:', str(e))
-finally:
-    # Cerrar la conexión con el servidor SMTP
-    server.quit()
+    # Enviar el correo electrónico
+    servidor_smtp.sendmail(remitente_real, destinatario, mensaje.as_string())
+
+    # Cerrar la conexión SMTP
+    servidor_smtp.quit()
+
+# Ejemplo de uso
+remitente_real = input("Correo electronico:")  # Tu dirección de correo electrónico real
+password = input("Contraseña:")  # Tu contraseña de correo electrónico
+servidor = input("Indica tu servidor SMTP:")
+remitente_falso = input("Indica correo electronico remitente falso:")  # Dirección de correo electrónico falsa
+destinatario = input("Indica destinatario:")  # Dirección de correo electrónico del destinatario
+asunto = input("Asunto de correo:")
+cuerpo = input("Texto:")
+
+
+enviar_correo_spoofed(destinatario, asunto, cuerpo, remitente_falso,remitente_real,password,servidor)
+print("Correo electrónico spoofed enviado exitosamente a", destinatario)
